cff0745aa1b0c9b7a1aecb8e73d559ebae22e2dd
Debarko De <debarko89@gmail.com>
2014-04-16 10:43:02 +0530

    Fix: Indentation

--- ios/MohoundPlugin.mm
+++ ios/MohoundPlugin.mm
@@ -7,16 +7,16 @@
 
 // The plugin must call super dealloc.
 - (void) dealloc {
-	[super dealloc];
+  [super dealloc];
 }
 
 // The plugin must call super init.
 - (id) init {
-	self = [super init];
-	if (!self) {
-		return nil;
-	}
-	return self;
+  self = [super init];
+  if (!self) {
+    return nil;
+  }
+  return self;
 }
 
 
@@ -30,9 +30,9 @@
 }
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-    [MohoundSDK initWithAppKey:@"d7d91de8f4b0ebd3827b6e6a5de57d77" andSecret:@"5f0f11008a430131c92b7efd45a4c1ef"];
-    [MohoundSDK setDebugMode:YES];
-    [MohoundSDK ping];
+  [MohoundSDK initWithAppKey:@"d7d91de8f4b0ebd3827b6e6a5de57d77" andSecret:@"5f0f11008a430131c92b7efd45a4c1ef"];
+  [MohoundSDK setDebugMode:YES];
+  [MohoundSDK ping];
 }
 
 - (void) handleOpenURL:(NSURL *)url sourceApplication:(NSString *)sourceApplication {
@@ -42,35 +42,35 @@
 }
 
 - (void) sendPayment:(NSDictionary *)jsonObject {
-    NSString *item =  [NSString stringWithFormat:@""];
-    NSNumber *price;
+  NSString *item =  [NSString stringWithFormat:@""];
+  NSNumber *price;
 
-    for (id key in jsonObject) {
-        id o = [jsonObject objectForKey:key];
-        if([key isEqual:@"item"]){
-            item = o;
-            continue;
-        }
-        if([key isEqual:@"price"]){
-            price = o;
-            continue;
-        }
+  for (id key in jsonObject) {
+    id o = [jsonObject objectForKey:key];
+    if([key isEqual:@"item"]){
+      item = o;
+      continue;
     }
+    if([key isEqual:@"price"]){
+      price = o;
+      continue;
+    }
+  }
 
-    [Mohound trackPurchaseWithUSDValue:price forItem:item];
+  [Mohound trackPurchaseWithUSDValue:price forItem:item];
 }
 
 - (void) sendEvent:(NSDictionary *)jsonObject {
-    NSString *name =  [NSString stringWithFormat:@""];
+  NSString *name =  [NSString stringWithFormat:@""];
 
-    for (id key in jsonObject) {
-        id o = [jsonObject objectForKey:key];
-        if([key isEqual:@"name"]){
-            name = o;
-            continue;
-        }
+  for (id key in jsonObject) {
+    id o = [jsonObject objectForKey:key];
+    if([key isEqual:@"name"]){
+      name = o;
+      continue;
     }
-    
-    [MohoundSDK trackAction:name];
+  }
+
+  [MohoundSDK trackAction:name];
 }
 @end
